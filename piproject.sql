b2cf5eed6c9bb3f8e080cbeb7d762831a896ee96
Bidetti <36907728+Bidetti@users.noreply.github.com>
2022-04-05 11:36:13 -0300

    Update piproject.sql

--- piproject.sql
+++ piproject.sql
@@ -4,13 +4,9 @@ create table user_informations(
 userID integer unique primary key not null auto_increment,
 userName varchar(22) unique not null,
 userPassword varchar(22) not null,
-userPermission varchar(14) not null default 'user',
-userStatus varchar(14) not null default 'false'
-);
-create table ranking_informations(
-userID integer unique not null auto_increment,
 userRank varchar(14) not null default 'ferro',
 userPoints integer not null default 0,
 userRanking integer default NULL,
-foreign key (userID) references user_informations(userID) on delete cascade on update cascade 
+userPermission varchar(14) not null default 'user',
+userStatus varchar(14) not null default 'false'
 );
